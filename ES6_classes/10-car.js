a123d9aa93bebd4c2b993ea11e8ee9ed1eba0672
vijaymallavalli <vijaysaradhim@brightcomgroup.com>
2023-03-10 07:03:12 +0000

    d

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -18,10 +18,3 @@ export default class Car {
 
   // Getters
 }
-/* class TestCar extends Car {};
-const tc1 = new TestCar("Nissan", "Turbo", "Pink");
-const tc2 = tc1.cloneCar();
-console.log(tc1 instanceof TestCar);
-console.log(tc2 instanceof TestCar);
-console.log(tc1 == tc2); */
-

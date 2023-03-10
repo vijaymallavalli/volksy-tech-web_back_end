484c68b3a07893a78a64c0e0d09d0669c0b5f816
vijaymallavalli <vijaysaradhim@brightcomgroup.com>
2023-03-10 05:47:13 +0000

    d

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,12 @@
+export default class Building {
+  constructor(sqft) {
+    // Any class extending Building must implement below method
+    if (this.constructor !== Building && !this.evacuationWarningMessage) throw Error('Class extending Building must override evacuationWarningMessage');
+
+    // Create objs
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }}
